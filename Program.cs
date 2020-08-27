48eb1f5ec5268c2387a402fc5a40bf8309feb28c
Dan Elliott <Daniel.Elliott2@va.gov>
2020-08-27 11:15:19 -0400

    setup coravel

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 namespace Sample
 {
+    using Coravel.Invocable;
+    using Coravel.Pro;
+    using Coravel.Pro.EntityFramework;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using System;
+    using System.Threading.Tasks;
+
     public class Program
     {
         public static void Main(string[] args)
@@ -22,5 +24,63 @@ namespace Sample
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase(databaseName: "JobBoard"));
+
+            services.AddRazorPages().AddNewtonsoftJson();
+
+            services.AddCoravelPro(typeof(ApplicationDbContext));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            context.Database.EnsureCreated();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapGet("/", async context =>
+                {
+                    await Task.Run(() => context.Response.Redirect(location: @"/Coravel/Scheduling", permanent: true));
+                });
+            });
+
+            app.UseCoravelPro();
+        }
+    }
+
+    public class ApplicationDbContext : DbContext, ICoravelProDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<CoravelJobHistory> Coravel_JobHistory { get; set; }
+        public DbSet<CoravelScheduledJob> Coravel_ScheduledJobs { get; set; }
+        public DbSet<CoravelScheduledJobHistory> Coravel_ScheduledJobHistory { get; set; }
+    }
+
+    public class MyFirstInvocable : IInvocable
+    {
+        public Task Invoke()
+        {
+            Console.WriteLine("This is my first invocable!");
+            return Task.CompletedTask;
+        }
     }
 }
